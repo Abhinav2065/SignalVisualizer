03cc2d4cdd66bc536358d6002c2c7c5b74e8830d
Abhinav2065 <abhinavsl511@gmail.com>
2025-10-19 02:00:34 +0545

    Added an example of digital Signal

--- SignalVisualizer/src/components/Home.jsx
+++ SignalVisualizer/src/components/Home.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 import '../styles/Home.css'
 import analogSignal from '../assets/Analog-Signal.png'
+import digitalSignal from '../assets/digitalSignal.png'
 
 
 const Home = () => {
@@ -17,6 +18,13 @@ const Home = () => {
           <p>A signal is any measurable quantity or information that varies over a dimension like time, space, or frequency.It can be a physical phenomenon, such as an electrical current or sound wave, or a non-physical concept, like a price or a message. Signals are used to convey information and can be represented mathematically as functions, with common types including continuous-time, discrete-time, analog, and digital signals.</p>
         </div>
       </div>
+
+      <div className="example">
+        Here is an example of a digital signal!
+        <br />
+        <img src={digitalSignal} alt="DigitalSignal" />
+      </div>
+
 
     </div>
   )
