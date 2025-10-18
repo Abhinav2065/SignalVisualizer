c461b7ed83e72385983ff45f163994ead0195ed1
Abhinav2065 <abhinavsl511@gmail.com>
2025-10-18 14:05:56 +0545

    made the image and text on the hero page better

--- SignalVisualizer/src/components/Home.jsx
+++ SignalVisualizer/src/components/Home.jsx
@@ -8,9 +8,14 @@ const Home = () => {
     <div>
       
       <div className="signals">
-        <h3>What are Signals?</h3>
-        <img src={analogSignal} alt="AnalogSignal" className='image'/>
-        <p>A signal is any measurable quantity or information that varies over a dimension like time, space, or frequency.It can be a physical phenomenon, such as an electrical current or sound wave, or a non-physical concept, like a price or a message. Signals are used to convey information and can be represented mathematically as functions, with common types including continuous-time, discrete-time, analog, and digital signals.</p>
+        <div className="image-section">
+          <img src={analogSignal} alt="AnalogSignal" className='image'/>
+        </div>
+
+        <div className="content-section">
+          <h3>What are Signals?</h3>
+          <p>A signal is any measurable quantity or information that varies over a dimension like time, space, or frequency.It can be a physical phenomenon, such as an electrical current or sound wave, or a non-physical concept, like a price or a message. Signals are used to convey information and can be represented mathematically as functions, with common types including continuous-time, discrete-time, analog, and digital signals.</p>
+        </div>
       </div>
 
     </div>
